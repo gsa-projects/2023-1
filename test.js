0ad6ea97945aac1800f646f8539daa1cba4c6f2e
Hyunseung Ryu <56152093+Rhseung@users.noreply.github.com>
2023-05-01 10:37:00 +0900

    .

--- test.js
+++
@@ -1,9 +0,0 @@
-const bot = BotManager.getCurrentBot();
-
-bot.addListener(Event.MESSAGE, msg => {
-    msg.isDualApp = msg.systemUserId == 95;
-
-    if (msg.room.name == '류현승' && msg.isDualApp) {
-        msg.reply(`isMention: ${msg.isMention}, hasMention: ${msg.hasMention}`);
-    }
-});
